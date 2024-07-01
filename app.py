d5613cc2af7066a56d6e525cc8e79bce2aba598c
kalanasamaraweera <kalanags331@gmail.com>
2024-07-01 09:36:32 +0530

    remote static graph

--- app.py
+++ app.py
@@ -5,14 +5,6 @@ import json
 # Create an undirected graph
 G = nx.Graph()
 
-# Add nodes
-G.add_node(1)
-G.add_nodes_from([2, 3, 4])
-
-# Add edges
-G.add_edge(1, 2)
-G.add_edges_from([(2, 3), (3, 4), (4, 1)])
-
 # Assign attributes
 G.nodes[1]['color'] = 'red'
 G[1][2]['weight'] = 5
